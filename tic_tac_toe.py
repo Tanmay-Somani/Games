78b0d90a75ee2a0d93121250aad1ce1cd4a78668
Tanmay Somani <tanmaysomani2003@gmail.com>
2023-08-23 13:31:18 +0530

    Add files via upload

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,55 +1,59 @@
 import random
-board={"tl":"","t":"","tr":"",
-           "ml":"","m":"","mr":"",
-           "ll":"","l":"","lr":""} 
-    
-def TheBoard(board):
-    print(board["tl"]+" | "+board["t"]+" | "+board["tr"])
+
+board = {
+    "tl": "", "t": "", "tr": "",
+    "ml": "", "m": "", "mr": "",
+    "ll": "", "l": "", "lr": ""
+}
+
+def print_board(board):
+    print(board["tl"] + " | " + board["t"] + " | " + board["tr"])
     print("- + - + -")
-    print(board["ml"]+" | "+board["m"]+" | "+board["mr"])
+    print(board["ml"] + " | " + board["m"] + " | " + board["mr"])
     print("- + - + -")
-    print(board["ll"]+" | "+board["l"]+" | "+board["lr"])
-choice=True
-TheBoard(board)
-while choice:
-    choice=input("Enter whether you want X or O ")
-    if choice == "X" or choice=="x":
-        choice="X"
-        break
-    elif choice == "O" or choice=="o" :
-        choice="O"
-        break
-    else:
-        print("choose again,Invalid choice")
+    print(board["ll"] + " | " + board["l"] + " | " + board["lr"])
+
+
+def choose_symbol():
+    while True:
+        choice = input("Enter whether you want X or O: ").upper()
+        if choice == "X" or choice == "O":
+            return choice
+        else:
+            print("Invalid choice, please choose again.")
+
+
+def input_user(symbol):
+    while True:
+        entry = input("Enter the area you want to place it in (1-9): ")
+        if entry in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            position = {
+                "1": "tl", "2": "t", "3": "tr",
+                "4": "ml", "5": "m", "6": "mr",
+                "7": "ll", "8": "l", "9": "lr"
+            }[entry]
+            if board[position] == "":
+                board[position] = symbol
+                break
+            else:
+                print("That position is already occupied. Choose another.")
+        else:
+            print("Invalid input. Please enter a number between 1 and 9.")
+
+
+def play_game():
+    player_symbol = choose_symbol()
+    computer_symbol = "X" if player_symbol == "O" else "O"
     
+    print("You chose:", player_symbol)
+    
+    while True:
+        print_board(board)
+        input_user(player_symbol)
+        print_board(board)
+        comp_entry = random.choice([pos for pos in board.keys() if board[pos] == ""])
+        board[comp_entry] = computer_symbol
+        print_board(board)
 
-comp_entry=random.randint(0,9)
-def input_user():
-    Entry=input("Enter the area you want to place it in")
-    if Entry=="1 " or Entry=="tl":
-        board["tl"]=choice
-    elif Entry=="2" or Entry=="t":
-        board["t"]=choice
-    elif Entry=="3" or Entry=="tr":
-        board["tr"]=choice
-    elif Entry=="4" or Entry=="ml":
-        board["ml"]=choice
-    elif Entry=="5" or Entry== "m":
-        board["m"]=choice 
-    elif Entry=="6" or Entry=="mr":
-        board["mr"]=choice
-    elif Entry=="7" or Entry== "ll":
-        board["ll"]=choice
-    elif Entry=="8" or Entry=="l":
-        board["l"]=choice
-    elif Entry=="9" or Entry=="lr":
-        board["lr"]=choice
-    else:
-        pass
 
-def setup():
-    for i in range(0,11):
-        input_user()
-        TheBoard(board)
-
-setup()
+play_game()
